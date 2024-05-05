6ccd1a5aae1c7fb1ef903856054a798c66c7161a
suttha jongjaroen <6410615121@cn330>
2024-05-05 15:33:14 +0700

    update Apple

--- Apple.java
+++ Apple.java
@@ -1,7 +1,9 @@
 public class Apple {
-    public int[] coordinate; 
+    public int coord_x; 
+    public int coord_y;
 
-    public Apple(int[] coordinate){
-        this.coordinate = coordinate;
+    public Apple(int coord_x, int coord_y){
+        this.coord_x = coord_x;
+        this.coord_y = coord_y;
     }
 }
