514fe44bd06386dabee20a3feb932cdfd30c9041
suttha jongjaroen <6410615121@cn330>
2024-05-05 11:07:38 +0700

    update main

--- Server.java
+++ Server.java
@@ -1,3 +1,7 @@
 public class Server {
-    
+    public static void main(String[] args) {
+        Snake snake = new Snake(new int[]{0,0});
+        snake.move("up");
+        snake.eat(new Apple(new int[]{0,0}));
+    }
 }
