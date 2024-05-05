ab347be10e1e67a11b297c2804e88b1ae29114f1
suttha jongjaroen <6410615121@cn330>
2024-05-05 10:13:43 +0700

    init Apple.java Server.java Snake.java

---
+++ Apple.java
@@ -0,0 +1,7 @@
+public class Apple {
+    private int[] coordinate; 
+
+    public Apple(int[] coordinate){
+        this.coordinate = coordinate;
+    }
+}
---
+++ Snake.java
@@ -0,0 +1,9 @@
+public class Snake {
+    private int length;
+    private int[] passed;
+
+    private void eat(Apple a){
+        length += 1;
+    }
+    
+}
