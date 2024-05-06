f7d9bf68be4c37e4564eb86bb33100595b603c33
suttha jongjaroen <6410615121@cn330>
2024-05-06 14:44:22 +0700

    implement Game.java

--- simpleBlock/Game.java
+++ simpleBlock/Game.java
@@ -1,5 +1,31 @@
 package simpleBlock;
 
-public class Game {
-    
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game implements Serializable{
+    private Board board;
+    private List<Player> players;
+
+    public Game(Board board) {
+        this.board = board;
+        players = new ArrayList<Player>();
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
+    }
+
+    public void delPlayer(Player player){
+        players.remove(player);
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
 }
