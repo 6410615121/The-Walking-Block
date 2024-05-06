ecb6794a7f3371742b1cb9bf5bf3fd5b766cbcea
suttha jongjaroen <6410615121@cn330>
2024-05-06 20:45:23 +0700

    update Client.java

--- simpleBlock/Client.java
+++ simpleBlock/Client.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -11,86 +12,53 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Client {
-    static void movePlayerRight(Player player,Board board){
-        Cell position = player.getPositionCell();
-        int current_col = position.getCol();
-        int current_row = position.getRol();
+    private Player player;
+    private List<Player> players;
+    private Board board;
 
-        current_col += 1;
-        if (current_col > board.COL_SIZE){
-            current_col = 0;
-        }
-        // System.out.printf("col: %d\n", current_col);
+    private Socket socket;
+    private ObjectInputStream inObject;
+    private ObjectOutputStream outObject;
 
-        player.setPositionCell(new Cell(current_row, current_col));
-        System.out.println(player.getPositionCell());
-        System.out.println("updated position");
-        // System.out.println(player);
-    }
-    public static void main(String[] args) {
-        Socket socket;
+    public Client(Player player, Socket socket) {
+        this.player = player;
+        this.socket = socket;
+
         try {
-            socket = new Socket("localhost", 12345);
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            String message = in.readUTF();
-            System.out.println("message from server: " + message);
-
-            // send Player
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            Player player = new Player("paipai323", "red", new Cell(5, 5));
-            out.writeObject(player);
-            out.flush();
-            // out.close();
-
-            // get board
-            ObjectInputStream inObj = new ObjectInputStream(socket.getInputStream());
-            Object obj = inObj.readObject();
-            Board board;
-            if (obj instanceof Board) {
-                board = (Board) obj;
-            } else {
-                socket.close();
-                throw new IOException("can't get Board");
-            }
-
-            while (true) {
-                // sent player
-                out.writeUnshared(player);
-                // System.out.println(player.hashCode());
-                System.out.println("sent player: " + player);
-
-                // recieve players
-                obj = inObj.readObject();
-                List<Player> players = null;
-                if(obj instanceof ArrayList){
-                    players = (ArrayList<Player>) obj;
-                }
-                System.out.println("got players");
-                movePlayerRight(player, board);
-                TimeUnit.MILLISECONDS.sleep(500);
-            }
-
-            // socket.close();
-        } catch (IOException | ClassNotFoundException | InterruptedException e) {
+            inObject = new ObjectInputStream(socket.getInputStream());
+            outObject = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.err.println("Error while trying to create Object...Stream");
             e.printStackTrace();
         }
     }
-    // public static void main(String[] args) {
-    // try{
-    // Socket socket = new Socket("localhost", 12345);
-    // ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-    // Object obj = in.readObject();
 
-    // if(obj instanceof Player){
-    // Player player = (Player) obj;
-    // System.out.println("recieved player color: " + player.getColor());
-    // }
+    public void sentPlayerObj() {
+        try {
+            outObject.writeUnshared(player);
+        } catch (IOException e) {
+            System.err.println("Error while trying to sent Player Object");
+            e.printStackTrace();
+        }
+    }
 
-    // socket.close();
+    public void getBoard(ObjectInputStream in) {
+        try {
+            this.board = (Board) in.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            System.err.println("Error while trying to get Board Object");
+            e.printStackTrace();
+        }
+    }
 
-    // }catch(IOException | ClassNotFoundException e){
-    // e.printStackTrace();
-    // }
-    // }
+    @SuppressWarnings("unchecked")
+    public void getPlayers() {
+        try {
+            this.players = (List<Player>) inObject.readObject();
+        } catch (ClassNotFoundException | IOException e) {
+            System.err.println("Error while trying to get players list");
+            e.printStackTrace();
+        }
+    }
 
 }
