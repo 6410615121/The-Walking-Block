d79b78d3bfece3cc26569e8ea86ccfcfb5b0eb18
suttha jongjaroen <6410615121@cn330>
2024-05-06 15:51:24 +0700

    test more of socket on handlerClient

--- simpleBlock/Client.java
+++ simpleBlock/Client.java
@@ -4,24 +4,39 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class Client {
     public static void main(String[] args) {
-        try{
+        try {
             Socket socket = new Socket("localhost", 12345);
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            Object obj = in.readObject();
-            
-            if(obj instanceof Player){
-                Player player = (Player) obj;
-                System.out.println("recieved player color: " + player.getColor());
-            }
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            String message = in.readUTF();
 
+            System.out.println("message from server: " + message);
             socket.close();
-            
-        }catch(IOException | ClassNotFoundException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    // public static void main(String[] args) {
+    //     try{
+    //         Socket socket = new Socket("localhost", 12345);
+    //         ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+    //         Object obj = in.readObject();
+            
+    //         if(obj instanceof Player){
+    //             Player player = (Player) obj;
+    //             System.out.println("recieved player color: " + player.getColor());
+    //         }
+
+    //         socket.close();
+            
+    //     }catch(IOException | ClassNotFoundException e){
+    //         e.printStackTrace();
+    //     }
+    // }
+    
 }
--- simpleBlock/GameServer.java
+++ simpleBlock/GameServer.java
@@ -7,22 +7,75 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameServer {
+    private List<Client> client;
+    private Game game;
+    public GameServer(){
+        client = new ArrayList<Client>();
+        game = new Game(new Board(10, 10));
+    }
+
+    static class ClientHandler implements Runnable{
+        private final Socket clientSocket;
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+        @Override
+        public void run() {
+            try {
+                DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+                out.writeUTF("hey Client");
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                try {
+                    clientSocket.close();
+                    System.out.println("Client disconnected: " + clientSocket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        try {
-            Player player = new Player("paipai323", "red", new Cell(0, 0));
+        try{
             ServerSocket serverSocket = new ServerSocket(12345);
 
-            System.out.println("waiting on 12345");
-            Socket clientSocket = serverSocket.accept();
+            while(true){
+                System.out.println("waiting for connection");
+                Socket clientSocket = serverSocket.accept();
 
-            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-            out.writeObject(player);
-            out.close();
-
+                //handle client
+                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                clientThread.start();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+
+    // public static void main(String[] args) {
+    //     try {
+    //         Player player = new Player("paipai323", "red", new Cell(0, 0));
+    //         ServerSocket serverSocket = new ServerSocket(12345);
+
+    //         System.out.println("waiting on 12345");
+    //         Socket clientSocket = serverSocket.accept();
+
+    //         ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+    //         out.writeObject(player);
+    //         out.close();
+
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 }
