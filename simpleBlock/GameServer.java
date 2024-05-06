0f67b4d07e3a00bbda0b11e057bde7194d21b8ea
suttha jongjaroen <6410615121@cn330>
2024-05-06 21:07:33 +0700

    implement more on GameServer.java

--- simpleBlock/GameServer.java
+++ simpleBlock/GameServer.java
@@ -13,27 +13,64 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class GameServer {
-    private Game game;
-    private List<Player> players;
+    private final Game game;
+
+    public GameServer(Game game){
+        this.game = game;
+    }
 
     public static class ClientHandler implements Runnable{
         private Socket clientSocket;
-        private Game game;
-        private List<Player> players;
-        private Board board;
+        private final Game game;
+        private final Board board;
 
+        private ObjectInputStream inObject;
+        private ObjectOutputStream outObject;
+
+        // constructer of ClientHandler
         public ClientHandler(Socket clientSocket, Game game){
             this.clientSocket = clientSocket;
             this.game = game;
             this.board = game.getBoard();
-            this.players = game.getPlayers();
+
+            try {
+                this.inObject = new ObjectInputStream(clientSocket.getInputStream());
+                this.outObject = new ObjectOutputStream(clientSocket.getOutputStream());
+            } catch (IOException e) {
+                System.err.println("Error while trying to init Object..Stream");
+                e.printStackTrace();
+            }
+            
+        }
+
+        private Player getPlayerObjectFromClient(){
+            try {
+                Player player = (Player) inObject.readObject();
+                return player;
+            } catch (ClassNotFoundException | IOException e) {
+                e.printStackTrace();
+            return null;
+            }
+        }
+
+        private void sentBoardToClient(){
+            try {
+                outObject.writeUnshared(board);
+            } catch (IOException e) {
+                System.err.println("Error while trying to send Board Object to client");
+                e.printStackTrace();
+            }
         }
 
         @Override
         public void run() {
             // get Player object
+            Player player = getPlayerObjectFromClient();
+
             // sent Board object
+            sentBoardToClient();
             
+
             // loop (get player > update pos > sent players)
         }
     }
