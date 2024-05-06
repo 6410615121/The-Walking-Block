ac0e948c82a7a166ecbe7adf03629711b6efd7cd
suttha jongjaroen <6410615121@cn330>
2024-05-06 15:27:43 +0700

    test Socket

--- simpleBlock/Client.java
+++ simpleBlock/Client.java
@@ -1,5 +1,27 @@
 package simpleBlock;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+
 public class Client {
-    
+    public static void main(String[] args) {
+        try{
+            Socket socket = new Socket("localhost", 12345);
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            Object obj = in.readObject();
+            
+            if(obj instanceof Player){
+                Player player = (Player) obj;
+                System.out.println("recieved player color: " + player.getColor());
+            }
+
+            socket.close();
+            
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
 }
--- simpleBlock/GameServer.java
+++ simpleBlock/GameServer.java
@@ -1,5 +1,28 @@
 package simpleBlock;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class GameServer {
-    
+    public static void main(String[] args) {
+        try {
+            Player player = new Player("paipai323", "red", new Cell(0, 0));
+            ServerSocket serverSocket = new ServerSocket(12345);
+
+            System.out.println("waiting on 12345");
+            Socket clientSocket = serverSocket.accept();
+
+            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            out.writeObject(player);
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
